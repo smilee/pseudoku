5e0e2fb0158b676586bda50ceb60b6bc1d8b4caa
Seungmin Lee <seungmin@uxplus.dev>
2021-03-01 13:14:39 +0900

    build: lint and build

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,72 @@ import { generateSudoku, generatePuzzle, updateGrid } from '../sudoku';
 import './style.css';
 
 export default function App() {
+  const Container = styled.div({
+    boxSizing: 'border-box',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: '20px',
+    width: '100%',
+  });
   const Sudoku = styled.div({
-    marginTop: '16%',
-    marginLeft: '50%',
+    margin: '10% auto 0',
     display: 'inline-grid',
-    gridTemplate: 'repeat(9, 60px) / repeat(9, 60px)',
-    transform: 'translateX(-50%)',
-    border: '5px solid #333333',
-  })
+    gridTemplate: 'repeat(9, 40px) / repeat(9, 40px)',
+    border: '4px solid #333333',
+  });
   const Cell = styled.div({
     textAlign: 'center',
     '&:nth-child(3n)': {
-      borderRight: '3px solid #333333',
+      borderRight: '2px solid #333333',
     },
     '&:nth-child(9n)': {
-      border: 'none'
+      border: 'none',
     },
     '&:nth-child(n+19):nth-child(-n+27)': {
-      borderBottom: '3px solid #4f4444',
+      borderBottom: '2px solid #4f4444',
     },
     '&:nth-child(n+46):nth-child(-n+54)': {
-      borderBottom: '3px solid #333333',
-    }
-  })
+      borderBottom: '2px solid #333333',
+    },
+  });
   const Input = styled.input({
-    marginTop: '3px',
-    padding: 0,
-    width: '48px',
-    fontSize: '48px',
+    display: 'block',
+    padding: '5px 0 0 2px',
+    width: '34px',
+    height: '30px',
+    fontSize: '36px',
     textAlign: 'center',
+    lineHeight: '26px',
+    verticalAlign: 'top',
     color: '#666666',
     border: 'none',
+    '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
+      WebkitAppearance: 'none',
+      display: 'none',
+    },
     '&:focus': {
-      outline: 'none'
-    }
+      outline: 'none',
+    },
+    '@supports (-webkit-touch-callout: none)': {
+      padding: '0 0 5px 2px',
+    },
   });
   const Given = styled.span({
+    boxSizing: 'border-box',
     width: 'fit-content',
     height: 'fit-content',
-    lineHeight: '60px',
+    lineHeight: '40px',
     fontFamily: "'Courier Prime', monospace",
-    fontSize: '48px',
+    fontSize: '36px',
     textAlign: 'center',
   });
   const Button = styled.button({
     display: 'block',
-    margin: '24px auto',
+    marginTop: '24px',
     padding: '16px 36px',
+    width: '368px',
     fontSize: '20px',
     fontWeight: 'bold',
     color: '#333333',
@@ -64,73 +83,76 @@ export default function App() {
     boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
     transition: 'box-shadow 0.1s ease-in-out',
     '&:hover': {
-      boxShadow: '0px 15px 20px rgba(0, 0, 0, 0.1)'
-    }
-  })
+      boxShadow: '0px 15px 20px rgba(0, 0, 0, 0.1)',
+    },
+  });
 
   const [state, setState] = useState({});
   useEffect(() => {
     const answer = generateSudoku(9);
-    const solution = generatePuzzle(answer)
-    setState({...state, answer, sudoku: solution, solution })
-  }, [])
-  const sudokuRef = useRef(null)
+    const solution = generatePuzzle(answer);
+    setState({
+      ...state, answer, sudoku: solution, solution,
+    });
+  }, []);
+  const sudokuRef = useRef(null);
 
   const { answer, sudoku, solution } = state;
 
   function handleChange(event, { row, column }) {
-    const { target: { value }} = event;
+    const { target: { value } } = event;
     const updatedSolution = updateGrid({
       grid: state.solution,
-      coordinates: {row, column},
-      value: parseInt(value)
+      coordinates: { row, column },
+      value: parseInt(value, 10),
     });
 
-    setState({...state, solution: updatedSolution })
+    setState({ ...state, solution: updatedSolution });
   }
 
   function handleClick(event) {
     event.preventDefault();
-
-    console.log('answer:', answer, 'solution:', solution)
     answer.forEach((row, i) => {
       row.forEach((cell, j) => {
         const target = sudokuRef.current.getElementsByClassName(`input-${i}-${j}`)[0];
-        console.log(target);
         if (solution[i][j] > 0 && cell !== solution[i][j]) {
-          console.log('answer:', cell, 'solution:', solution[i][j])
-          console.log('value:', target.value)
           target.style.color = '#bc544b';
         }
-      })
-    })
+      });
+    });
   }
 
   return (
-    <>
+    <Container>
       <Sudoku title="sudoku" ref={sudokuRef}>
         {
           sudoku?.map((row, i) => (
             row.map((cell, j) => (cell === 0
-              ? <Cell
+              ? (
+                <Cell
                   key={`input-${i}-${j}`}
                 >
                   <Input
-                    type="text"
+                    type="number"
+                    inputmode="numeric"
                     className={`input-${i}-${j}`}
                     value={solution[i][j] > 0 ? solution[i][j] : ''}
                     onChange={((event) => handleChange(event, { row: i, column: j }))}
                   />
                 </Cell>
-              : <Cell
-                  key={`cell-${i}-${j}`}>
+              )
+              : (
+                <Cell
+                  key={`cell-${i}-${j}`}
+                >
                   <Given>{ cell }</Given>
-                </Cell>))
+                </Cell>
+              )))
           ))
         }
-        
+
       </Sudoku>
       <Button type="button" onClick={handleClick}>Check solution</Button>
-    </>
+    </Container>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: './main.js',
   },
   module: {
